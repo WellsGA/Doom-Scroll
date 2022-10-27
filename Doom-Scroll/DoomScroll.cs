83d68a3bcc4117839cd9ab739f8bb123e1ff4a65
Romhányi Ágnes <romhanyi.agi@gmail.com>
2022-10-27 09:10:53 -0700

    base plugin

---
+++ Doom-Scroll/DoomScroll.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace Doom_Scroll;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class DoomScroll : BasePlugin
+{
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# DoomScrollV2
+
+An Among Us mod about misinformation.
+
+This is alternative version that doesn't use Reactor.
+Find the original project repo here: https://github.com/WellsGA/Doom-Scroll-Among-Us-mod
+
