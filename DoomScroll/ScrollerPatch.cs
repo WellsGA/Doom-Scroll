910490ce30348973ffe514a47e3ea8883d54aef1
AlainaAK <alaina.klaes@gmail.com>
2022-11-22 13:22:49 -0800

    added Chat Scroller mods (disabled scroller limit)

---
+++ DoomScroll/ScrollerPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using Doom_Scroll.Common;
+
+namespace Doom_Scroll
+{
+    //Disable chat scrolling limit:
+    [HarmonyPatch(typeof(Scroller), nameof(Scroller.Update))]
+    public static class ScrollerUpdatePatch
+    {
+        private static bool m_ScrollerPatchHasRun = false;
+        public static void Postfix(Scroller __instance)
+        {
+
+            if (!m_ScrollerPatchHasRun)
+            {
+                __instance.ContentYBounds = new FloatRange(float.MinValue, float.MaxValue);
+            }
+        }
+    }
+}
