cba357f183b09bfa8275331a641a0a1ad43a9c20
AlainaAK <alaina.klaes@gmail.com>
2023-02-13 13:37:22 -0800

    Changed entry point for sending SWCs

--- DoomScroll/EndGameManagerPatch.cs
+++ DoomScroll/EndGameManagerPatch.cs
@@ -15,6 +15,8 @@ namespace Doom_Scroll
         public static void PrefixSetEverythingUp(EndGameManager __instance)
         {
             SecondaryWinCondition.Evaluate();
+            DoomScroll._log.LogInfo("Sending PLACEHOLDER RPC!");
+            RPCManager.RPCSendSWCSuccessText("PLACEHOLDER");
         }
         
         [HarmonyPostfix]
@@ -22,9 +24,7 @@ namespace Doom_Scroll
         public static void PostfixSetEverythingUp(EndGameManager __instance)
         {
             __instance.WinText.text += "\n<size=20%><color=\"white\"> { SWC Results } " + SecondaryWinCondition.SWCResultsText() + "</color></size>";
-            DoomScroll._log.LogInfo("Sending PLACEHOLDER RPC!");
             // once RPC verified working, "PLACEHOLDER" will be SecondaryWinCondition.sendableResultsText()
-            RPCManager.RPCSendSWCSuccessText("PLACEHOLDER");
             DoomScroll._log.LogInfo("After sending RPC, playerSWClist = " + SecondaryWinCondition.overallSWCResultsText());
         }
     }
