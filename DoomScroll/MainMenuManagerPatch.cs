dec9fae8587cfed567ae769aed8fe343ce414220
AlainaAK <alaina.klaes@gmail.com>
2023-05-24 13:38:21 -0700

    BUTTON ON MAIN SCREEN WORKING!!!

--- DoomScroll/MainMenuManagerPatch.cs
+++ DoomScroll/MainMenuManagerPatch.cs
@@ -9,7 +9,6 @@ using Il2CppSystem.Text;
 using Doom_Scroll.UI;
 using System.Reflection;
 using UnityEngine.UI;
-using Sentry.Internal;
 
 namespace Doom_Scroll
 {
@@ -20,26 +19,16 @@ namespace Doom_Scroll
         public static CustomButton test_button;
         public static CustomModal credits_overlay;
         public static CustomButton close_button;
-        public static DialogueBox Dialogue = new DialogueBox();
-        public static CreditsScreenPopUp our_credits = new();
-        public static AspectPosition aspect = new AspectPosition();
         public static bool AreCreditsOpen { get; private set; }
 
-        public static void ShowPopUp(string text)
+        public static void OpenLink()
         {
-            Dialogue.Show(text);
+            Application.OpenURL("https://www.google.com/");
         }
 
         private static MainMenuManager mainMenuManagerInstance;
-        
-        [HarmonyPrefix]
-        [HarmonyPatch("LateUpdate")]
-        public static void PrefixLateUpdate()
-        {
-            CheckButtonClicks();
-        }
 
-
+        /*
         [HarmonyPostfix]
         [HarmonyPatch("Start")]
         public static void PostfixStart(MainMenuManager __instance)
@@ -51,7 +40,7 @@ namespace Doom_Scroll
             GameObject inventoryButton = GameObject.Find("InventoryButton").gameObject;
             Vector3 doomscrollBtnPos = inventoryButton.gameObject.transform.position;
             SpriteRenderer doomscrollButtonSr = inventoryButton.GetComponent<SpriteRenderer>();
-            Vector3 position = new Vector3(doomscrollBtnPos.x - doomscrollButtonSr.size.x * inventoryButton.transform.localScale.x, doomscrollBtnPos.y, doomscrollBtnPos.z-70);
+            Vector3 position = new Vector3(doomscrollBtnPos.x - doomscrollButtonSr.size.x * inventoryButton.transform.localScale.x, doomscrollBtnPos.y, doomscrollBtnPos.z - 70);
             Vector2 scaledSize = doomscrollButtonSr.size * inventoryButton.transform.localScale;
             scaledSize = scaledSize / 2;
             Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
@@ -62,10 +51,10 @@ namespace Doom_Scroll
             test_button.ButtonEvent.MyAction += OnClickDoomScroll;
             test_button.ActivateCustomUI(true);
 
-            credits_overlay = InitCreditsOverlay(GameObject.Find("MainUI").gameObject);
-            close_button = AddCloseButton(credits_overlay.UIGameObject);
-            close_button.ButtonEvent.MyAction += ToggleOurCredits;
-        }
+            //credits_overlay = InitCreditsOverlay(GameObject.Find("MainUI").gameObject);
+            //close_button = AddCloseButton(credits_overlay.UIGameObject);
+            //close_button.ButtonEvent.MyAction += ToggleOurCredits;
+        }*/
         public static CustomModal InitCreditsOverlay(GameObject parent)
         {
             CustomModal creditsOverlay = CreateCreditsOverlay(parent);
@@ -93,7 +82,7 @@ namespace Doom_Scroll
             return new CustomButton(parent, "Close OurCredits", closeBtnImg, position, buttonSize.x);
         }
 
-        public static void CheckButtonClicks()
+        /*public static void CheckButtonClicks()
         {
             if (mainMenuManagerInstance == null) return;
             //DoomScroll._log.LogMessage("Replacing hover image");
@@ -127,12 +116,7 @@ namespace Doom_Scroll
                     DoomScroll._log.LogError("Error invoking overlay button method: " + e);
                 }
             }
-        }
-
-        public static void OnClickDoomScroll()
-        {
-            ToggleOurCredits();
-        }
+        }*/
         public static void ToggleOurCredits()
         {
             if (AreCreditsOpen)
@@ -151,6 +135,86 @@ namespace Doom_Scroll
                 AreCreditsOpen = true;
                 DoomScroll._log.LogInfo("opening");
             }
+        }
+    
+
+        [HarmonyPrefix]
+        [HarmonyPatch("LateUpdate")]
+        public static void PrefixLateUpdate(MainMenuManager __instance)
+        {
+            CheckButtonClicks();
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Start")]
+        public static void PostfixStart(MainMenuManager __instance)
+        {
+            AreCreditsOpen = false;
+            mainMenuManagerInstance = __instance;
+
+            //GameObject m_UIParent = __instance.playerCustomizationPrefab.transform.parent.gameObject;
+            GameObject m_UIParent = __instance.DefaultButtonSelected.transform.parent.gameObject.transform.Find("BottomButtons").gameObject;
+            GameObject inventoryButton = m_UIParent.transform.Find("InventoryButton").gameObject;
+            Vector3 doomscrollBtnPos = inventoryButton.gameObject.transform.position;
+            SpriteRenderer doomscrollButtonSr = inventoryButton.GetComponent<SpriteRenderer>();
+            Vector3 position = new Vector3(doomscrollBtnPos.x - doomscrollButtonSr.size.x * inventoryButton.transform.localScale.x, doomscrollBtnPos.y, doomscrollBtnPos.z);
+            Vector2 scaledSize = doomscrollButtonSr.size * inventoryButton.transform.localScale;
+            scaledSize = scaledSize / 2;
+            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+            Sprite[] doomscrollBtnSprites = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "Doom_Scroll.Assets.MainMenu_Button_Green.png", slices);
+
+
+            test_button = new CustomButton(m_UIParent, "DoomScroll Info Toggle Button", doomscrollBtnSprites, position, scaledSize.x);
+
+            test_button.ActivateCustomUI(true);
+
+            test_button.ButtonEvent.MyAction += OnClickDoomScroll;
+
+            credits_overlay = InitCreditsOverlay(GameObject.Find("MainUI").gameObject);
+            close_button = AddCloseButton(credits_overlay.UIGameObject);
+            close_button.ButtonEvent.MyAction += ToggleOurCredits;
+
+        }
+        public static void CheckButtonClicks()
+        {
+            if (mainMenuManagerInstance == null) return;
+
+            test_button.ReplaceImgageOnHover();
+
+            try
+            {
+                // Invoke methods on mouse click - open DoomScroll info popup
+                if (test_button.isHovered() && Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    test_button.ButtonEvent.InvokeAction();
+                }
+            }
+            catch (System.Exception e)
+            {
+                DoomScroll._log.LogError("Error invoking method: " + e);
+            }
+
+            try
+            {
+                if (close_button.isHovered() && Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    close_button.ButtonEvent.InvokeAction();
+                }
+            }
+            catch (System.Exception e)
+            {
+                DoomScroll._log.LogError("Error invoking overlay button method: " + e);
+            }
+        }
+
+        public static void OnClickDoomScroll()
+        {
+            //test_button.EnableButton(false);
+            ToggleOurCredits();
+            OpenLink();
+            //DestroyableSingleton<HudManager>.Instance.ShowPopUp(DestroyableSingleton<TranslationController>.Instance.GetString(StringNames.GameOverTaskWin, Array.Empty<object>()));
+            //mainMenuManagerInstance.ShowPopUp(DestroyableSingleton<TranslationController>.Instance.GetString(StringNames.GameOverTaskWin, Array.Empty<object>()));
         }
     }
 }
