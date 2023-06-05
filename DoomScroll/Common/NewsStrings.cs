941d2e6456fe8ca154e20f1e7b38212f3d649963
dannyao97 <dannyao97@gmail.com>
2023-06-05 12:25:19 -0700

    Update NewsStrings.cs
    Added new NewsStrings and Sources

--- DoomScroll/Common/NewsStrings.cs
+++ DoomScroll/Common/NewsStrings.cs
@@ -1,4 +1,7 @@
 ﻿
+using Hazel.Dtls;
+using static Il2CppSystem.Xml.Schema.XsdDuration;
+
 namespace Doom_Scroll.Common
 {
     internal static class NewsStrings
@@ -12,13 +15,17 @@ namespace Doom_Scroll.Common
             "{0} Isn't Doing Their Fair Share!",
             "{0} Caught Stealing Lunches from Fridge",
             "/'{0} CANNOT Be Trusted/', Local Ghost Warns",
-            "HERO: {0} Saves Ship from Sabotage"
+            "HERO: {0} Saves Ship from Sabotage",
+            "{0} Parties 'Til 4am, Keeps Shipmates Awake",
+            "{0} Caught Loitering Near Vents"
         };
 
         public static string[] headlines2p = new string[]
         {
             "{0} seen arguing with {1}",
             "{0} and {1} Spotted Holding Hands During Meeting?!",
+            "{0} Blames {1} for Slow Task Completion",
+            "{0} and {1} Caught Sharing Headphones at Lunch"
         };
 
         public static string[] headlines1p1n = new string[]
@@ -32,19 +39,38 @@ namespace Doom_Scroll.Common
             "Amogus Weekly",
             "Bluffington Post",
             "Crewmate Community Board",
-            "SlothEater Chronicles",
+            "ZotZotZot Chronicles",
             "Julia the Journalist",
+            "Crews News",
+            "The Gopher Report"
         };
 
         public static string[] fakeSources = new string[]
         {
             "Old Task Archives",
-            "SuspiciousMinds.sloth",
+            "SuspiciousMinds Website",
             "Red Tribunal",
             "Innocent Street Journal",
             "Spacehip News Network",
             "Imposter Kyle/'s Personal Blog",
-            "@NotMe29"
+            "@NotMe29",
+            "Bluffington Post",
+            "@GLSNews",
+            "The Selfless Report"
+        };
+
+        public static string[] PropagandaSources = new string[]
+        {
+            "Minnie from Accounting",
+            "AmonGossip",
+            "Bean Scene Magazine",
+            "Polus Weekly",
+            "The Vent Sesh",
+            "From the Abyss",
+            "Brian's Room",
+            "Pasta Sauce Post",
+            "MeetingNotes Website",
+            "The Ejected Editorial"
         };
     }
 }
