0817395f881178e557f7e8e4c8c7a4af1aa474aa
AlainaAK <alaina.klaes@gmail.com>
2023-06-27 16:14:26 -0700

    Fixed the errors yayyy ;-;
    (Added an if statement. That's why such a big chunk of code looks changed.)

--- DoomScroll/MMOnlineManagerPatch.cs
+++ DoomScroll/MMOnlineManagerPatch.cs
@@ -87,71 +87,74 @@ namespace Doom_Scroll
         [HarmonyPatch("Start")]
         public static void PostfixStart(MMOnlineManager __instance)
         {
-            DoomScroll._log.LogInfo("About to set MMOnlineManager instance...");
-            AreCreditsOpen = false;
-            MMOnlineManagerInstance = __instance;
+            if (__instance.HelpMenu != null && __instance.HelpMenu.gameObject.activeSelf == false)
+            {
+                DoomScroll._log.LogInfo("About to set MMOnlineManager instance...");
+                AreCreditsOpen = false;
+                MMOnlineManagerInstance = __instance;
 
-            DoomScroll._log.LogInfo("About to set up for credits toggle button...");
-            DoomScroll._log.LogInfo("Getting BackButton...");
-            GameObject m_UIParent = __instance.BackButton.transform.gameObject;
-            DoomScroll._log.LogInfo("Getting HelpButton from BackButton...");
-            GameObject helpButton = m_UIParent.transform.parent.transform.Find("HelpButton").gameObject;
-            DoomScroll._log.LogInfo("Got HelpButton");
-            Vector3 doomscrollBtnPos = helpButton.gameObject.transform.position;
-            SpriteRenderer doomscrollButtonSr = helpButton.GetComponent<SpriteRenderer>();
-            Vector3 position = new Vector3(doomscrollBtnPos.x+3, doomscrollBtnPos.y, doomscrollBtnPos.z - 10);
-            Vector2 scaledSize = doomscrollButtonSr.size * helpButton.transform.localScale;
-            //scaledSize = scaledSize / 2;
-            Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
-            Sprite[] doomscrollBtnSprites = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "Doom_Scroll.Assets.MainMenu_Button_Green.png", slices);
+                DoomScroll._log.LogInfo("About to set up for credits toggle button...");
+                DoomScroll._log.LogInfo("Getting BackButton...");
+                GameObject m_UIParent = __instance.BackButton.transform.gameObject;
+                DoomScroll._log.LogInfo("Getting HelpButton from BackButton...");
+                GameObject helpButton = m_UIParent.transform.parent.transform.Find("HelpButton").gameObject;
+                DoomScroll._log.LogInfo("Got HelpButton");
+                Vector3 doomscrollBtnPos = helpButton.gameObject.transform.position;
+                SpriteRenderer doomscrollButtonSr = helpButton.GetComponent<SpriteRenderer>();
+                Vector3 position = new Vector3(doomscrollBtnPos.x + 3, doomscrollBtnPos.y, doomscrollBtnPos.z - 10);
+                Vector2 scaledSize = doomscrollButtonSr.size * helpButton.transform.localScale;
+                //scaledSize = scaledSize / 2;
+                Vector4[] slices = { new Vector4(0, 0.5f, 1, 1), new Vector4(0, 0, 1, 0.5f) };
+                Sprite[] doomscrollBtnSprites = ImageLoader.ReadImageSlicesFromAssembly(Assembly.GetExecutingAssembly(), "Doom_Scroll.Assets.MainMenu_Button_Green.png", slices);
 
-            DoomScroll._log.LogInfo("About to create credits toggle button...");
-            test_button = new CustomButton(m_UIParent, "DoomScroll Info Toggle Button", doomscrollBtnSprites, position, scaledSize.x);
-            DoomScroll._log.LogInfo("Credits toggle button created");
+                DoomScroll._log.LogInfo("About to create credits toggle button...");
+                test_button = new CustomButton(m_UIParent, "DoomScroll Info Toggle Button", doomscrollBtnSprites, position, scaledSize.x);
+                DoomScroll._log.LogInfo("Credits toggle button created");
 
-            test_button.ActivateCustomUI(false);
-            test_button.ActivateCustomUI(true);
-            test_button.UIGameObject.gameObject.SetActive(false);
-            test_button.UIGameObject.gameObject.SetActive(true);
+                test_button.ActivateCustomUI(false);
+                test_button.ActivateCustomUI(true);
+                test_button.UIGameObject.gameObject.SetActive(false);
+                test_button.UIGameObject.gameObject.SetActive(true);
 
-            test_button.ButtonEvent.MyAction += OnClickDoomScroll;
+                test_button.ButtonEvent.MyAction += OnClickDoomScroll;
 
-            DoomScroll._log.LogInfo("About to create credits overlay...");
-            credits_overlay = CreateCreditsOverlay(GameObject.Find("NormalMenu").gameObject);
-            DoomScroll._log.LogInfo("Credits overlay created");
-            close_button = AddCloseButton(credits_overlay.UIGameObject);
-            DoomScroll._log.LogInfo("Added close button");
-            close_button.ButtonEvent.MyAction += ToggleOurCredits;
+                DoomScroll._log.LogInfo("About to create credits overlay...");
+                credits_overlay = CreateCreditsOverlay(GameObject.Find("NormalMenu").gameObject);
+                DoomScroll._log.LogInfo("Credits overlay created");
+                close_button = AddCloseButton(credits_overlay.UIGameObject);
+                DoomScroll._log.LogInfo("Added close button");
+                close_button.ButtonEvent.MyAction += ToggleOurCredits;
 
-            //<<CREATE CREDITS TEXT>>
-            string creditsText = "DoomScroll is an Among Us mod intended to shape the gameplay towards developing the player’s\n" +
-                " ability to identify and avoid misinformation. The game’s core mechanics are already well built\n" +
-                " for creating social deception – players must listen to each other’s insights and perspectives\n" +
-                " and determine who might not be telling the truth, lest they choose incorrectly and let the imposter\n" +
-                " win. Our goal is to add even greater depth to this process by introducing elements commonly found\n" +
-                " when assessing misinformation on social media. Some of these elements include:" +
-                "\r\n\r\n<b>Secondary Win Conditions (SWC):</b> \nAdditional objectives for crewmates. Frame a rival player or protect an ally!" +
-                "\r\n<b>Chat logs:</b> \nchat logs are no longer removed after each meeting, \nallowing you look back at everyone’s actions from earlier in the game." +
-                "\r\n<b>Sign-in Forms:</b> \nWhile completing a task, sign in as another player to make it appear as if they completed it." +
-                "\r\n<b>Headlines:</b> \nPlayers will randomly be given the chance to share a news headline with the rest of the group. \nThese hints might reveal the truth, or just create mass confusion!" +
-                "\r\n<b>Folder System:</b> \nThe folder system allows you to pull up evidence (sign-in forms and headlines)\n during meetings to support your arguments." +
-                "\r\n\r\n<b>DoomScroll Development Team:</b>" +
-                "\r\n\r\n-                                                      Garrison Wells - Designer, Producer \t\t\t Alaina Klaes - Programmer, Artist                                        -" +
-                "\r\n\r\n-                                                          Agnes Romhanyi - Lead Programmer \t\t\t    Ashley Chia Sun - Intern                                                          -" +
-                "\r\n\r\nTwitter: @DoomScrollMod";
-            CustomText credits_text = new CustomText(credits_overlay.UIGameObject, "DoomScrollTeamCredits", creditsText);
-            credits_text.SetColor(Color.black);
-            credits_text.SetSize(3f);
-            Vector3 textPos = new Vector3(0, credits_overlay.GetSize().x / 2 + 0.5f - 3f, -10);
-            credits_text.SetLocalPosition(textPos);
-            credits_text.TextMP.m_enableWordWrapping = false;
+                //<<CREATE CREDITS TEXT>>
+                string creditsText = "DoomScroll is an Among Us mod intended to shape the gameplay towards developing the player’s\n" +
+                    " ability to identify and avoid misinformation. The game’s core mechanics are already well built\n" +
+                    " for creating social deception – players must listen to each other’s insights and perspectives\n" +
+                    " and determine who might not be telling the truth, lest they choose incorrectly and let the imposter\n" +
+                    " win. Our goal is to add even greater depth to this process by introducing elements commonly found\n" +
+                    " when assessing misinformation on social media. Some of these elements include:" +
+                    "\r\n\r\n<b>Secondary Win Conditions (SWC):</b> \nAdditional objectives for crewmates. Frame a rival player or protect an ally!" +
+                    "\r\n<b>Chat logs:</b> \nchat logs are no longer removed after each meeting, \nallowing you look back at everyone’s actions from earlier in the game." +
+                    "\r\n<b>Sign-in Forms:</b> \nWhile completing a task, sign in as another player to make it appear as if they completed it." +
+                    "\r\n<b>Headlines:</b> \nPlayers will randomly be given the chance to share a news headline with the rest of the group. \nThese hints might reveal the truth, or just create mass confusion!" +
+                    "\r\n<b>Folder System:</b> \nThe folder system allows you to pull up evidence (sign-in forms and headlines)\n during meetings to support your arguments." +
+                    "\r\n\r\n<b>DoomScroll Development Team:</b>" +
+                    "\r\n\r\n-                                                      Garrison Wells - Designer, Producer \t\t\t Alaina Klaes - Programmer, Artist                                        -" +
+                    "\r\n\r\n-                                                          Agnes Romhanyi - Lead Programmer \t\t\t    Ashley Chia Sun - Intern                                                          -" +
+                    "\r\n\r\nTwitter: @DoomScrollMod";
+                CustomText credits_text = new CustomText(credits_overlay.UIGameObject, "DoomScrollTeamCredits", creditsText);
+                credits_text.SetColor(Color.black);
+                credits_text.SetSize(3f);
+                Vector3 textPos = new Vector3(0, credits_overlay.GetSize().x / 2 + 0.5f - 3f, -10);
+                credits_text.SetLocalPosition(textPos);
+                credits_text.TextMP.m_enableWordWrapping = false;
 
-            //<<CREATE LINK BUTTON>>
-            SpriteRenderer sr = credits_overlay.UIGameObject.GetComponent<SpriteRenderer>();
-            Vector3 link_button_pos = textPos + new Vector3(0, -5.8f, 0);
-            Sprite[] closeBtnImg = { ImageLoader.ReadImageFromAssembly(Assembly.GetExecutingAssembly(), "Doom_Scroll.Assets.closeButton.png") };
-            link_button = new CustomButton(credits_overlay.UIGameObject, "Close OurCredits", doomscrollBtnSprites, link_button_pos, buttonSize.x);
-            link_button.ButtonEvent.MyAction += OpenLink;
+                //<<CREATE LINK BUTTON>>
+                SpriteRenderer sr = credits_overlay.UIGameObject.GetComponent<SpriteRenderer>();
+                Vector3 link_button_pos = textPos + new Vector3(0, -5.8f, 0);
+                Sprite[] closeBtnImg = { ImageLoader.ReadImageFromAssembly(Assembly.GetExecutingAssembly(), "Doom_Scroll.Assets.closeButton.png") };
+                link_button = new CustomButton(credits_overlay.UIGameObject, "Close OurCredits", doomscrollBtnSprites, link_button_pos, buttonSize.x);
+                link_button.ButtonEvent.MyAction += OpenLink;
+            }
 
         }
         public static void CheckButtonClicks()
