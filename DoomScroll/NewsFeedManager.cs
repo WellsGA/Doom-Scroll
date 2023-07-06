fc7f160f68c9765c2572a5ca84329b9dd1ee55dc
Romhányi Ágnes <romhanyi.agi@gmail.com>
2023-07-06 13:52:19 -0700

    task text and rpc notification

--- DoomScroll/NewsFeedManager.cs
+++ DoomScroll/NewsFeedManager.cs
@@ -249,8 +249,6 @@ namespace Doom_Scroll
         public void RPCShareNews(NewsItem news)
         {
             AddNews(news); // add locally
-            NotificationManager.ShowNotification("News posted\n " + news.Title + " [" + news.Source + "]");
-
             MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SENDNEWS, (SendOption)1);
             messageWriter.Write(news.Author);
             messageWriter.Write(news.Title);
@@ -261,6 +259,7 @@ namespace Doom_Scroll
 
         public void AddNews(NewsItem news) 
         {
+            NotificationManager.ShowNotification("News posted\n " + news.Title + " [" + news.Source + "]");
             allNewsList.Add(news);
         }
 
--- DoomScroll/TaskAssigner.cs
+++ DoomScroll/TaskAssigner.cs
@@ -92,7 +92,7 @@ namespace Doom_Scroll
                 {
                     AssignedTask assTask = new AssignedTask(task.Id, task.TaskType, sender.PlayerId, playerID);
                     AssignedTasks.Add(assTask);
-                    NotificationManager.ShowNotification("New task assigned to " + assTask.AssigneeName);
+                    NotificationManager.ShowNotification(assTask.AssigneeName + " signed into a task.");
                     DoomScroll._log.LogInfo("New task assigned\n player: " + assTask.AssigneeName + ", task: " + assTask.Type);
                 }
             }
