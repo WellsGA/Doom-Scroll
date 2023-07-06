6456421b6260fb49626f22a644d283e28d1fafa5
AlainaAK <alaina.klaes@gmail.com>
2023-07-06 12:18:34 -0700

    Discussion time tooltip!

--- DoomScroll/MeetingHudPatch.cs
+++ DoomScroll/MeetingHudPatch.cs
@@ -1,15 +1,27 @@
-﻿using HarmonyLib;
+﻿using AmongUs.GameOptions;
+using Doom_Scroll.UI;
+using HarmonyLib;
+using UnityEngine;
 
 namespace Doom_Scroll
 {
     [HarmonyPatch(typeof(MeetingHud))]
     static class MeetingHudPatch
     {
+        public static CustomText currentToolTipText;
+        public static MeetingHud meetingHudInstance;
+
         [HarmonyPostfix]
         [HarmonyPatch("Update")]
         public static void PostfixUpdate()
         {
             FolderManager.Instance.CheckForButtonClicks();
+            if (currentToolTipText.TextMP.text != "" && meetingHudInstance.state != MeetingHud.VoteStates.Discussion && meetingHudInstance.state != MeetingHud.VoteStates.Animating)
+            {
+                currentToolTipText.TextMP.text = "";
+                currentToolTipText.ActivateCustomUI(false);
+                DoomScroll._log.LogInfo($"MeetingHud state is {meetingHudInstance.state}. Text should be deactivated!");
+            }
         }
         [HarmonyPostfix]
         [HarmonyPatch("Close")]
@@ -17,6 +29,20 @@ namespace Doom_Scroll
         {
             FolderManager.Instance.CloseFolderOverlay();
         }
-
+        [HarmonyPostfix]
+        [HarmonyPatch("Start")]
+        public static void PostfixStart(MeetingHud __instance)
+        {
+            meetingHudInstance = __instance;
+            DoomScroll._log.LogInfo("Meeting Hud starting! Trying to add tooltip.");
+            GameObject uiParent = __instance.TitleText.gameObject;
+            currentToolTipText = new CustomText(uiParent, "DiscussionTimeTooltip", "Use this time to look through the files in the folder!\n<size=50%>Open the chat, and click the folder button with a paperclip on it.</size>");
+            currentToolTipText.SetColor(Color.yellow);
+            currentToolTipText.SetSize(3f);
+            Vector3 textPos = new Vector3(0, -3.5f, -10);
+            currentToolTipText.SetLocalPosition(textPos);
+            currentToolTipText.ActivateCustomUI(true);
+            DoomScroll._log.LogInfo("Text should be activated!");
+        }
     }
 }
