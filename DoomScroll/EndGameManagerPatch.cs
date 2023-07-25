9d876009de7e281c1ad961390b03b756d3aff61c
AlainaAK <alaina.klaes@gmail.com>
2023-07-25 11:19:40 -0700

    SWC requirement added back in, with color details

--- DoomScroll/EndGameManagerPatch.cs
+++ DoomScroll/EndGameManagerPatch.cs
@@ -17,13 +17,17 @@ namespace Doom_Scroll
         [HarmonyPatch("SetEverythingUp")]
         public static void PostfixSetEverythingUp(EndGameManager __instance)
         {
-            /*if (__instance.WinText.color != Color.red && !SecondaryWinConditionManager.LocalPLayerSWC.CheckSuccess()) //If won but SWC not successful
+            if (__instance.WinText.color != Color.red && !SecondaryWinConditionManager.LocalPLayerSWC.CheckSuccess()) //If won but SWC not successful
             {
                 StatsManager.Instance.AddLoseReason(TempData.EndReason);
                 __instance.WinText.text = DestroyableSingleton<TranslationController>.Instance.GetString(StringNames.Defeat, (Il2CppSystem.Object[])Array.Empty<object>());
                 __instance.WinText.color = Color.red;
-            }*/
-            __instance.WinText.text += "\n<size=20%><color=\"white\"> { SWC Results } " + SecondaryWinConditionManager.LocalPLayerSWC.SWCResultsText() + "</color></size>";
+                __instance.WinText.text += "\n<size=20%><color=\"white\"> { SWC Results } <color=\"red\">" + SecondaryWinConditionManager.LocalPLayerSWC.SWCResultsText() + "</color></color></size>";
+            }
+            else
+            {
+                __instance.WinText.text += "\n<size=20%><color=\"white\"> { SWC Results } <color=\"cyan\">" + SecondaryWinConditionManager.LocalPLayerSWC.SWCResultsText() + "</color></color></size>";
+            }
         }
     }
 }
