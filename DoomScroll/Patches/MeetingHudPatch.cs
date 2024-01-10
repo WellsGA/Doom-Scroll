6efd37a031af12f71968990a7b2640554f501460
AlainaAK <alaina.klaes@gmail.com>
2024-01-10 14:17:06 -0800

    Add CastVote prefix! Should now skip all votes before final RPC is sent out.

--- DoomScroll/Patches/MeetingHudPatch.cs
+++ DoomScroll/Patches/MeetingHudPatch.cs
@@ -5,6 +5,8 @@ using Hazel;
 using UnityEngine;
 using System;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Doom_Scroll.Patches
 {
@@ -13,6 +15,84 @@ namespace Doom_Scroll.Patches
     {
         public static Tooltip meetingBeginningToolTip;
         private static PlayerVoteArea[] playerVoters;
+
+        private static Dictionary<byte, int> DoomCalculateVotes(MeetingHud __instance)
+        {
+            Dictionary<byte, int> dictionary = new Dictionary<byte, int>();
+            for (int i = 0; i < __instance.playerStates.Length; i++)
+            {
+                PlayerVoteArea playerVoteArea = __instance.playerStates[i];
+                if (playerVoteArea.VotedFor != 252 && playerVoteArea.VotedFor != 255 && playerVoteArea.VotedFor != 254)
+                {
+                    int num;
+                    if (dictionary.TryGetValue(playerVoteArea.VotedFor, out num))
+                    {
+                        dictionary[playerVoteArea.VotedFor] = num + 1;
+                    }
+                    else
+                    {
+                        dictionary[playerVoteArea.VotedFor] = 1;
+                    }
+                }
+            }
+            return dictionary;
+        }
+        private static void DoomForceSkipAll(MeetingHud __instance)
+        {
+            for (int i = 0; i < __instance.playerStates.Length; i++)
+            {
+                PlayerVoteArea playerVoteArea = __instance.playerStates[i];
+                if (!playerVoteArea.DidVote)
+                {
+                    playerVoteArea.VotedFor = 254;
+                    __instance.DirtyBits |= 1U;
+                }
+            }
+        }
+
+        public static KeyValuePair<byte, int> DoomMaxPair(Dictionary<byte, int> self, out bool tie)
+        {
+            tie = true;
+            KeyValuePair<byte, int> result = new KeyValuePair<byte, int>(byte.MaxValue, int.MinValue);
+            foreach (KeyValuePair<byte, int> keyValuePair in self)
+            {
+                if (keyValuePair.Value > result.Value)
+                {
+                    result = keyValuePair;
+                    tie = false;
+                }
+                else if (keyValuePair.Value == result.Value)
+                {
+                    tie = true;
+                }
+            }
+            return result;
+        }
+        public static void DoomCastVote(MeetingHud __instance, byte srcPlayerId, byte suspectPlayerId)
+        {
+            GameData.PlayerInfo playerById = GameData.Instance.GetPlayerById(srcPlayerId);
+            GameData.PlayerInfo playerById2 = GameData.Instance.GetPlayerById(suspectPlayerId);
+            __instance.logger.Debug(playerById.PlayerName + " has voted for " + ((playerById2 != null) ? playerById2.PlayerName : "No one"), null);
+            PlayerVoteArea playerVoteArea = null;
+            foreach (PlayerVoteArea pv in __instance.playerStates)
+            {
+                if (pv.TargetPlayerId == srcPlayerId)
+                {
+                    playerVoteArea = pv;
+                    break;
+                }
+            }
+            if (!playerVoteArea.AmDead)
+            {
+                if (PlayerControl.LocalPlayer.PlayerId == srcPlayerId || AmongUsClient.Instance.NetworkMode != NetworkModes.LocalGame)
+                {
+                    SoundManager.Instance.PlaySound(__instance.VoteLockinSound, false, 1f, null);
+                }
+                playerVoteArea.SetVote(suspectPlayerId);
+                __instance.SetDirtyBit(1U);
+                //PlayerControl.LocalPlayer.RpcSendChatNote(srcPlayerId, ChatNoteTypes.DidVote);
+            }
+        }
 
         [HarmonyPrefix]
         [HarmonyPatch("Update")]
@@ -126,21 +206,89 @@ namespace Doom_Scroll.Patches
             ScreenshotManager.Instance.EnableCameraButton(false); // disable camera even if no picture was taken
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch("CastVote")]
-        public static void PostFixCastVote(byte srcPlayerId, byte suspectPlayerId)
+
+        [HarmonyPrefix]
+        [HarmonyPatch("PrefixCastVote")]
+        public static bool PrefixCastVote(MeetingHud __instance, byte srcPlayerId, byte suspectPlayerId)
         {
-            
             if (AmongUsClient.Instance.AmHost)
             {
                 // Log votes
                 string voter = GameData.Instance.GetPlayerById(srcPlayerId) == null ? "some one" : GameData.Instance.GetPlayerById(srcPlayerId).PlayerName;
                 string suspect = GameData.Instance.GetPlayerById(suspectPlayerId) == null ? "no one" : GameData.Instance.GetPlayerById(suspectPlayerId).PlayerName;
                 GameLogger.Write(GameLogger.GetTime() + " - " + voter + " has voted for " + suspect);
-                
-                // check for vote results if all-1 player had voded
+
+                // Code for changing vote stuff!
+
+                // check if this is the last vote
+                PlayerVoteArea thisVoteArea = null;
+                foreach (PlayerVoteArea playerVoteArea in __instance.playerStates)
+                {
+                    if (playerVoteArea.TargetPlayerId == srcPlayerId)
+                    {
+                        thisVoteArea = playerVoteArea;
+                    }
+                    else if (!playerVoteArea.AmDead && !playerVoteArea.DidVote) // someone else is alive and didn't vote
+                    {
+                        return true; // stops the prefix, continues on with the method as expected
+                    }
+                }
+
+                // means that this is the only person who hasn't voted!! Time to calculate and replace votes...
+                thisVoteArea.SetVote(suspectPlayerId);
+                Dictionary<byte, int> dictionary = DoomCalculateVotes(__instance); // Calculates votes after locally setting whatever this current player's vote should be
+
+                //their code in CheckForEndVoting. array2, exiled, and tie are the parameters that go into RpcVotingComplete.
+                bool tie;
+                KeyValuePair<byte, int> max = DoomMaxPair(dictionary, out tie);
+                Logger logger = __instance.logger;
+                string format = "Vote counts: {0} Max={1}@{2} Tie={3}";
+                object[] array = new object[4];
+                array[0] = string.Join(" ", (from t in dictionary
+                                             select t.ToString()).ToArray<string>());
+                array[1] = max.Key;
+                array[2] = max.Value;
+                array[3] = tie;
+                logger.Debug(string.Format(format, array), null);
+                GameData.PlayerInfo exiled = null;
+                foreach (GameData.PlayerInfo v in GameData.Instance.AllPlayers)
+                {
+                    if (!tie && v.PlayerId == max.Key)
+                    {
+                        exiled = v;
+                        break;
+                    }
+                }
+                MeetingHud.VoterState[] array2 = new MeetingHud.VoterState[__instance.playerStates.Length];
+                for (int i = 0; i < __instance.playerStates.Length; i++)
+                {
+                    PlayerVoteArea playerVoteArea = __instance.playerStates[i];
+                    array2[i] = new MeetingHud.VoterState
+                    {
+                        VoterId = playerVoteArea.TargetPlayerId,
+                        VotedForId = playerVoteArea.VotedFor
+                    };
+                }
+                //their code ends!
+                ExileControllerPatch.OriginalArray2 = array2;
+                ExileControllerPatch.OriginalExiledPlayer = exiled;
+                ExileControllerPatch.OriginalTie = tie;
+
+                //Now we're setting everyone's votes to SKIP
+                //Using code from CastVote
+                foreach (GameData.PlayerInfo v in GameData.Instance.AllPlayers)
+                {
+                    if (!v.IsDead)
+                    {
+                        DoomCastVote(__instance, v.PlayerId, PlayerVoteArea.SkippedVote);
+                    }
+                }
+
+                // Now check for end voting, and cancel actual method.
+                __instance.CheckForEndVoting();
             }
-            HeadlineCreator.UpdatePlayerVote(srcPlayerId, suspectPlayerId);
+
+            return true;
         }
 
 
